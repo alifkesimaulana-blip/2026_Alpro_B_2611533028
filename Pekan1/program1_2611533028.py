4bebfa710fafb0368425445ebea434d02b6c2753
alifkesimaulana-blip <alifkesimaulana@gmail.com>
2026-09-03 10:39:09 +0700

    Add files via upload

---
+++ Pekan1/program1_2611533028.py
@@ -0,0 +1,12 @@
+# Program pertama
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 pratikum alpro
+"""
+print("Hello World")
+print()
+print("Program ini mengahasilkan")
+print(" Empat baris output")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini" + " dibuat oleh Alif Maulana Kesi_2611533028")
